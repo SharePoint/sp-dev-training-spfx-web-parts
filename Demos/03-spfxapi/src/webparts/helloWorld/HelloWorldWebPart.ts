f1fe26fc2594e9ee6b536700128352df6bef541c
Andrew Connell <me@andrewconnell.com>
2023-05-05 15:03:40 -0400

    FY23Q4 content refresh
    - run through all exercise steps in associated MSLearn module to validate good working order
    - updated steps & fixed typos where necessary
    - updated slides where necessary
    - update code samples to reflect MSLearn HOL exercise refresh & updates
    - any & all users/names/company names/pictures/IDs are not real people/tenants... all are from M365 demo tenants provided by Microsoft

--- Demos/03-spfxapi/src/webparts/helloWorld/HelloWorldWebPart.ts
+++ Demos/03-spfxapi/src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -1,6 +1,3 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
 import {
   Version,
   DisplayMode,
@@ -41,24 +38,24 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
     setTimeout(() => {
       this.context.statusRenderer.clearLoadingIndicator(this.domElement);
       this.domElement.innerHTML = `
-      <section class="${styles.helloWorld} ${!!this.context.sdks.microsoftTeams ? styles.teams : ''}">
-        <div class="${styles.welcome}">
-          <img alt="" src="${this._isDarkTheme ? require('./assets/welcome-dark.png') : require('./assets/welcome-light.png')}" class="${styles.welcomeImage}" />
-          <h2>Well done, ${escape(this.context.pageContext.user.displayName)}!</h2>
-          <div>${this._environmentMessage}</div>
-          <div>Web part property value: <strong>${escape(this.properties.description)}</strong></div>
-          <div>Site title: <strong>${escape(siteTitle)}</strong></div>
-          <div>Page mode: <strong>${escape(pageMode)}</strong></div>
-          <div>Environment: <strong>${escape(environmentType)}</strong></div>
-        </div>
-        <div>
-          <h3>Welcome to SharePoint Framework!</h3>
-          <p>
-          The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It's the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-          </p>
-          <button type="button">Show welcome message</button>
-        </div>
-      </section>`;
+    <section class="${styles.helloWorld} ${!!this.context.sdks.microsoftTeams ? styles.teams : ''}">
+      <div class="${styles.welcome}">
+        <img alt="" src="${this._isDarkTheme ? require('./assets/welcome-dark.png') : require('./assets/welcome-light.png')}" class="${styles.welcomeImage}" />
+        <h2>Well done, ${escape(this.context.pageContext.user.displayName)}!</h2>
+        <div>${this._environmentMessage}</div>
+        <div>Web part property value: <strong>${escape(this.properties.description)}</strong></div>
+        <div>Site title: <strong>${escape(siteTitle)}</strong></div>
+        <div>Page mode: <strong>${escape(pageMode)}</strong></div>
+        <div>Environment: <strong>${escape(environmentType)}</strong></div>
+      </div>
+      <div>
+        <h3>Welcome to SharePoint Framework!</h3>
+        <p>
+        The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It's the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
+        </p>
+        <button type="button">Show welcome message</button>
+      </div>
+    </section>`;
 
       this.domElement.getElementsByTagName("button")[0]
         .addEventListener('click', (event: MouseEvent) => {
@@ -74,19 +71,37 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
   }
 
   protected onInit(): Promise<void> {
-    this._environmentMessage = this._getEnvironmentMessage();
-
-    return super.onInit();
+    return this._getEnvironmentMessage().then(message => {
+      this._environmentMessage = message;
+    });
   }
 
 
 
-  private _getEnvironmentMessage(): string {
-    if (!!this.context.sdks.microsoftTeams) { // running in Teams
-      return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
+  private _getEnvironmentMessage(): Promise<string> {
+    if (!!this.context.sdks.microsoftTeams) { // running in Teams, office.com or Outlook
+      return this.context.sdks.microsoftTeams.teamsJs.app.getContext()
+        .then(context => {
+          let environmentMessage: string = '';
+          switch (context.app.host.name) {
+            case 'Office': // running in Office
+              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOffice : strings.AppOfficeEnvironment;
+              break;
+            case 'Outlook': // running in Outlook
+              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOutlook : strings.AppOutlookEnvironment;
+              break;
+            case 'Teams': // running in Teams
+              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
+              break;
+            default:
+              throw new Error('Unknown host');
+          }
+
+          return environmentMessage;
+        });
     }
 
-    return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment;
+    return Promise.resolve(this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment);
   }
 
   protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
